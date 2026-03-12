221210e146561614421f78e30b4c43636c30f32b
jhuliangr <jhpiano9731@gmail.com>
2026-03-13 00:20:36 +0100

    configured vite for css modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,31 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: process.env.BASE_URL ?? '/',
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName:
+        mode === 'test' || process.env.VITEST
+          ? '[local]'
+          : '[local]_[hash:base64:6]',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test-setup.ts'],
     testTimeout: 15000,
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped',
+      },
+    },
   },
   resolve: {
     alias: {
       '#shared': '/src/shared',
     },
   },
-});
+}));
